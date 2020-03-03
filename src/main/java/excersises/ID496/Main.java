67b7c4e951cf0732c353c9cfeefeea8fdf7e9ef0
Michal Koperski <mkoperpno@gmali.com>
2020-03-03 23:02:09 +0100

    finisk excersises ID496

--- src/main/java/excersises/ID496/Main.java
+++ src/main/java/excersises/ID496/Main.java
@@ -1,7 +1,52 @@
 package excersises.ID496;
 
-public class Main {
-    public static void main(String[] args) {
-        
+import java.util.Scanner;
+class Main
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        int a,d,j;
+        Scanner wczyt = new Scanner(System.in);
+        a=wczyt.nextInt();
+        int liczba[]=new int[a];
+        for(int i=0;i<a;i++){
+            liczba[i]=wczyt.nextInt();
+        }
+
+        for(int x:liczba){
+            switch(x){
+                case 0:
+                case 1:
+                    d=0;j=1;
+                    break;
+                case 2:
+                    d=0;j=2;
+                    break;
+                case 3:
+                    d=0;j=6;
+                    break;
+                case 4:
+                    d=2;j=4;
+                    break;
+                case 5:
+                    d=2;j=0;
+                    break;
+                case 6:
+                    d=2;j=0;
+                    break;
+                case 7:
+                    d=4;j=0;
+                    break;
+                case 8:
+                    d=2;j=0;
+                    break;
+                case 9:
+                    d=8;j=0;
+                    break;
+                default:
+                    d=j=0;
+            }
+            System.out.println(d+" "+j);
+        }
     }
 }
